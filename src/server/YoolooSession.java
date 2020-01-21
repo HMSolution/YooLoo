2e3828554cbcb13fc09ca3226927c947bdabfb2c
mousavim <mousavim@pc>
2020-01-21 09:49:20 +0100

    I changed a value

--- src/server/YoolooSession.java
+++ src/server/YoolooSession.java
@@ -44,7 +44,6 @@ public class YoolooSession {
 			YoolooKarte[] karten = spielplan[stichNummer];
 			for (int spielernummer = 0; spielernummer < spielplan[stichNummer].length; spielernummer++) {
 				if (spielplan[stichNummer][spielernummer] == null) {
-					karten = null;
 				}
 			}
 			if (karten != null) {
