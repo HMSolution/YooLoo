ebad68c387998be46115cf91f9ead4d17b960c80
HMSolution <leon.rengshausen@hauertmann.net>
2020-01-23 15:36:50 +0100

    Changed SocketUtils naming

--- src/utils/socketutils.java
+++ src/utils/socketutils.java
@@ -5,7 +5,7 @@ import java.io.ObjectOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
-public class socketutils {
+public class SocketUtils {
 
     public static void sendSerialized(Socket s, Serializable object) {
         try {
